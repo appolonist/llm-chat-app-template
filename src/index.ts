925194bc1dc603bfa7b8e57d6e193f2413e32a0a
Karol Piotrowski <50790817+appolonist@users.noreply.github.com>
2025-07-30 23:08:11 +0100

    fix: change link to lllm

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,7 @@ async function handleChatRequest(
     formData.append("q", prompt);
 
     // Send POST request to the external LLM API
-    const apiResponse = await fetch("https://someting.where.llm.live", {
+    const apiResponse = await fetch("https://llm-chat-app-template.karolpiotrowski90.workers.dev", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
